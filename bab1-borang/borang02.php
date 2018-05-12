aadb37d4f6320897359f83f41ece91aed1d5f673
Makmal IT HEBHK <kopikauhangit@gmail.com>
2018-05-12 15:05:36 +0800

    papar sql

--- bab1-borang/borang02.php
+++ bab1-borang/borang02.php
@@ -1,14 +1,25 @@
 <?php
 
-echo '$_POST:<pre>';
+/*echo '$_POST:<pre>';
 print_r($_POST);
-echo'</pre>';
+echo'</pre>';*/
 
 include 'DB_Pdo.php';
 include 'tatarajah.php';
 $db_Pdo = new \Aplikasi\Kitab\DB_Pdo(DB_TYPE, DB_HOST, DB_NAME, DB_USER, DB_PASS);
+
+#istihar pembolehubah
+$username=$_POST['login']['name'];
+$password=$_POST['login']['password'];
 # panggil sql
-$sql='select * from belajar';
+
+$sql='select * from belajar ' . "\r"
+     . 'where username like "' . $username . '" '
+	 . 'and password like "'. $password.'" ';	  
+echo '$sql:<pre>';
+print_r($sql);
+echo'</pre>'; 
+	  
 $result=$db_Pdo->selectAll($sql);
 
 echo '$result:<pre>';
