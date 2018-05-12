3fc209d33ffc2ba001e0a8028ec4e8fa8722208e
Makmal IT HEBHK <kopikauhangit@gmail.com>
2018-05-12 12:50:42 +0800

    ubah borang2.php

--- bab1-borang/borang02.php
+++ bab1-borang/borang02.php
@@ -1,5 +1,16 @@
 <?php
 
-echo '<pre>';
+echo '$_POST:<pre>';
 print_r($_POST);
 echo'</pre>';
+
+include 'DB_Pdo.php';
+include 'tatarajah.php';
+$db_Pdo = new \Aplikasi\Kitab\DB_Pdo(DB_TYPE, DB_HOST, DB_NAME, DB_USER, DB_PASS);
+# panggil sql
+$sql='select * from belajar';
+$result=$db_Pdo->selectAll($sql);
+
+echo '$result:<pre>';
+print_r($result);
+echo'</pre>'; 
